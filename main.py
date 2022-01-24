9ccb1cba3920622a5ccdd32be93815ea7b0a3ad6
Ayah Sena <haimisu949@gmail.com>
2022-01-25 02:36:58 +0800

    belajar

--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 """
-ini adalah belajar pertama python suhaimi
+Ibu menyuruh anak membeli telur
 """
-print("hello dunia")
-print("namaku suhaimi")
+print('Ibu  : " Din kewarung gin nukar susu 1 botol, mun JIKA ada telur nukar jua 6 biji telur')
+print('Udin : Oke ibu,')
+print('*'*15)
+print('Udin Pergi Ketoko')
+print('*'*10)
+print('Udin : Mang Jali adakah pian bejual susu?')
+#Amang jali mengecek susu ada/tidak
+susu_ada = True
+if susu_ada:
+    print('Mang Jali : Ada ai nah Susu, berapa botol km nukar?')
+    print('Udin : 1 botol ja mang ai')
+    print('Mang Jali : Nukar napa lagi?')
+    print('Udin : Telur adalah mang bejual? mun ada nukar 6 biji lun')
+else:
+    print('Mang Jali : Susu habis din ai, naikan wahini susu. wkwk')
+    print('Udin : Oke mang jali, mun kaitu bulik ai lun nah kd jadi beilanja')
+
+#Amang jali mencari Telur
+intalu_ada = True
+if intalu_ada:
+    print('Mang Jali : Ada Nah telurnya 6 biji')
+    print('Udin : Berapa total duitnya mang?')
+    print('1 botol susu = Rp.5000, 6 biji telur = Rp.10000')
+    print('Udin : Jadi totalnya')
+    print(10000+5000)
+    print('Mang Jali : Iya banr, harat jua eh km behitung kelas brp sekolah? wkwk')
+else:
+    print('Mang Jali : Kosong intalunya')
+    print('Udin : Mun kaitu lun nukar susu ja, berapa duitnya nih')
+    print('Mang jali : 5000 ja hargnya')
+
+
+#Udin bulik kerumah
+print('Nah ma intalu wn susunya')
+
+
+
+
+
